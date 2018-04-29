2443ed8bb017cea8b7a7c8bb81dbcf415f9016e6
Tim Humphries <tim@utf8.me>
2018-04-28 20:56:55 -0700

    Test a binding

--- src/FoundationDb/C.hs
+++ src/FoundationDb/C.hs
@@ -7,18 +7,14 @@ module FoundationDb.C (
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as B
 
-import           Foreign
-
 import qualified FoundationDb.C.FFI as FFI
 import           FoundationDb.C.Types
 
 import           System.IO.Unsafe (unsafePerformIO)
 
 
-errorMessage :: Error -> Maybe ByteString
+errorMessage :: Error -> ByteString
 errorMessage (Error cint) =
   unsafePerformIO $ do
     pt <- FFI.fdb_get_error cint
-    if pt == nullPtr
-      then return Nothing
-      else Just <$> B.packCString pt
+    B.packCString pt
--- test/Test/FoundationDb/C.hs
+++ test/Test/FoundationDb/C.hs
@@ -1,12 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeApplications #-}
 module Test.FoundationDb.C where
 
+
+import           Foreign.C
+
+import           FoundationDb.C
+import           FoundationDb.C.Types
 
 import           Test.QuickCheck
 
 
-prop_false :: Property
-prop_false = True === False
+prop_errorMessage_noSegfault :: Property
+prop_errorMessage_noSegfault =
+  forAll (arbitrary @ CInt) $ \cint ->
+    errorMessage (Error cint) `seq` property True
+
+prop_errorMessage_1 :: Property
+prop_errorMessage_1 =
+  once $
+    errorMessage (Error 1)
+    ===
+    "End of stream"
 
 
 return []
