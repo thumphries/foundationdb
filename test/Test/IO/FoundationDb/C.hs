9ec75643a4d7f03ba49641298691d19943c739ed
Tim Humphries <tim@utf8.me>
2018-05-19 14:06:22 -0700

    Add basic get/set test

--- test/Test/IO/FoundationDb/C.hs
+++ test/Test/IO/FoundationDb/C.hs
@@ -3,6 +3,8 @@
 {-# LANGUAGE TypeApplications #-}
 module Test.IO.FoundationDb.C where
 
+import qualified FoundationDb.C as C
+
 import           Test.FoundationDb.Util (testIO)
 import           Test.IO.FoundationDb.Process (withTestDb)
 import           Test.QuickCheck
@@ -16,8 +18,13 @@ prop_true =
 
 prop_foo :: Property
 prop_foo =
-  once . testIO . withTestDb $ \_cluster _database -> do
-    return ()
+  once . testIO . withTestDb $ \_cluster database -> do
+    txn <- C.createTransaction database
+    C.transactionSet txn "foobar" "bazquux"
+    ftr <- C.transactionGet txn "foobar" False
+    C.futureBlockUntilReady ftr
+    val <- C.futureGetValue ftr
+    return $ val === pure "bazquux"
 
 return []
 tests :: IO Bool
